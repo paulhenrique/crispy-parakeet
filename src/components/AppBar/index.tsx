13baa7cc6665e49846e1ac0dcd0b7c44d4cc3892
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2022-08-06 15:38:36 -0300

    Adding persist

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -4,16 +4,17 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { changeRoute } from "../../store/features/routerReducer";
-import { Container } from "@mui/material";
+import { Avatar, Container, IconButton, Menu, MenuItem } from "@mui/material";
 import { RootState } from "../../store";
 import { doLogout } from "../../store/features/userReducer";
+import { useState } from "react";
 
 export default function AppBar() {
   const dispatch = useDispatch();
   const changeRouteDispatch = (route: string) => dispatch(changeRoute(route));
   const { currentUser } = useSelector((state: RootState) => state.users);
   const logout = () => dispatch(doLogout());
-
+  const [open, setOpen] = useState(false);
   return (
     <Box sx={{ flexGrow: 1 }} mb="79px">
       <AppBarMui position="fixed">
@@ -41,9 +42,21 @@ export default function AppBar() {
               </>
             )}
             {currentUser && (
-              <Button color="inherit" onClick={() => logout()}>
-                Sair
-              </Button>
+              <>
+                <IconButton onClick={() => setOpen((current) => !current)}>
+                  <Avatar src={currentUser.picture} />
+                </IconButton>
+                <Button color="inherit" onClick={() => logout()}>
+                  Sair
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  open={open}
+                  onClose={() => setOpen(false)}
+                >
+                  <MenuItem onClick={() => logout()}>Logout</MenuItem>
+                </Menu>
+              </>
             )}
           </Toolbar>
         </Container>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,23 @@
 import { configureStore } from '@reduxjs/toolkit'
 import routerReducer from './features/routerReducer'
 import userReducer from './features/userReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
 
 export const store = configureStore({
   reducer: {
     router: routerReducer,
-    users: userReducer
+    users: persistReducer(persistConfig, userReducer)
   },
 })
 
+export const persistor = persistStore(store);
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
